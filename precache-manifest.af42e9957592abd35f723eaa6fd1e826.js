de16131da6603aa2100471c26c6c5695d1070350
davidizzle <davide.ferretti8498@gmail.com>
2021-11-22 14:34:15 +0100

    Updates

---
+++ precache-manifest.af42e9957592abd35f723eaa6fd1e826.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ad35029c3cc161566474c0d2ec6b005",
+    "url": "/v2/index.html"
+  },
+  {
+    "revision": "f44685805e76a4f888d9",
+    "url": "/v2/static/css/main.a962670d.chunk.css"
+  },
+  {
+    "revision": "6cbffb841f3dbfce4fa5",
+    "url": "/v2/static/js/2.e14b1b8c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/v2/static/js/2.e14b1b8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f44685805e76a4f888d9",
+    "url": "/v2/static/js/main.43418393.chunk.js"
+  },
+  {
+    "revision": "7fcc16caba2f6f6b1609",
+    "url": "/v2/static/js/runtime-main.3cbce2f2.js"
+  }
+]);
